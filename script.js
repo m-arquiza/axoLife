a3ecf3069482f86fc5f6717aba32f44e24fc2e69
Michelle Arquiza <marquiza@cs.washington.edu>
2022-10-22 16:21:23 -0700

    add: working timer

--- script.js
+++ script.js
@@ -1,5 +1,68 @@
+/*  *.* GLOBAL VARS
+    timers: is timer on, time timer is set to
+*/
+let timerOn = false;
+let timeSet;
+let timeLength = 1500;
+
+/*  *.* TIMER FUNCTIONS
+    timer set, start and end buttons
+*/
+
+/* variable function to set timer.*/
+setInterval(function() {
+    if(timerOn === true) {
+        // time right now
+        let now = new Date().getTime();
+        // time difference
+        let diff = timeSet - now;
+
+        // display time
+        document.getElementById("time-display").innerHTML = milliseconds_toString(diff)
+
+        // end timer
+        if (diff < 0) {
+            clearInterval();
+            document.getElementById("time-display").innerHTML = milliseconds_toString(timeLength * 1000);
+            timerOn = false;
+            localStorage.clear();
+        }
+    }
+}, 1000);
+
+/* variable function to start timer on start .*/
+document.getElementsByClassName("start")[0].addEventListener("click", function(){
+    localStorage.clear();
+    timerOn = true;
+    timeSet = new Date(new Date().getTime() + timeLength * 1000);
+});
+
+/* variable function to end timer on end .*/
+document.getElementsByClassName("end")[0].addEventListener("click", function(){
+    localStorage.clear();
+    timerOn = false;
+
+    document.getElementById("time-display").innerHTML = milliseconds_toString(timeLength * 1000);
+});
 /*  *.* SPACER TITLE
     gen title
 */
 
 /* indiv title.*/
+
+
+/*  *.* SPACER TITLE
+    gen title
+*/
+
+/* indiv title.*/
+
+
+/*  *.* HELPER FUNCTIONS
+    time conversion
+*/
+
+/* convert milliseconds to mins and seconds as a formatted string.*/
+function milliseconds_toString(time){
+    return (Math.floor((time/1000)/60) + ":" + Math.floor((time/1000)%60).toLocaleString(undefined,{minimumIntegerDigits: 2}))
+}
