7447580cd601d0f7420f294a5f8b8e9cdb80f18d
Michelle Arquiza <marquiza@cs.washington.edu>
2022-10-23 03:25:15 -0700

    add: popup opens and closes add: axo nap

--- script.js
+++ script.js
@@ -53,51 +53,56 @@ document.getElementsByClassName("end")[0].addEventListener("click", function(){
 /* sidebar function add event listeners.*/
 document.getElementsByClassName("home-button")[0].addEventListener("click", function(){
     document.getElementsByClassName("axolotl")[0].src = "images/axolotl/home-axolotl.gif";
-    document.getElementsByClassName("mode-text")[0].innerHTML = "welcome back!";
+    document.getElementsByClassName("mode-text")[0].innerHTML = "welcome back~";
 });
 document.getElementsByClassName("study-button")[0].addEventListener("click", function(){
     document.getElementsByClassName("axolotl")[0].src = "images/axolotl/study-axolotl.gif";
-    document.getElementsByClassName("mode-text")[0].innerHTML = "it's time to grind!";
+    document.getElementsByClassName("mode-text")[0].innerHTML = "it's time to grind~";
 });
 document.getElementsByClassName("nom-button")[0].addEventListener("click", function(){
     //document.getElementsByClassName("axolotl")[0].src = "images/axolotl/nom-axolotl.gif";
-    document.getElementsByClassName("mode-text")[0].innerHTML = "nom nom!";
+    document.getElementsByClassName("mode-text")[0].innerHTML = "nom nom~";
 });
 document.getElementsByClassName("nap-button")[0].addEventListener("click", function(){
-    //document.getElementsByClassName("axolotl")[0].src = "images/axolotl/nap-axolotl.gif";
-    document.getElementsByClassName("mode-text")[0].innerHTML = "sleep tight!";
+    document.getElementsByClassName("axolotl")[0].src = "images/axolotl/nap-axolotl.gif";
+    document.getElementsByClassName("mode-text")[0].innerHTML = "sleep tight~";
 });
 
 
 /*  *.* POPUP FUNCTIONS
     close and open popups, accept form submissions
 */
-/* initial name popup.*/
+/* initial name- popup and set name.*/
 document.getElementById("name-submit").addEventListener("click", function(){
     let checkName = document.getElementById("name-value").value;
     if(!(checkName.length <= 12 && checkName.length > 0)){
         document.getElementsByClassName("initialPop")[0].style.display = "block";
     }else{
         name = checkName;
-        document.getElementsByClassName("hello-text")[0].innerHTML = "hello, " + name + " :)";
+        document.getElementsByClassName("hello-text")[0].innerHTML = "hello, " + name + "!";
         document.getElementById("initial-popup").style.display = "none";
     }
 });
 
-/* open popup on gear click.*/
+/* setting- open popup on gear click.*/
 document.getElementsByClassName("gear")[0].addEventListener("click", function(){
     document.getElementById("settings-popup").style.display = "block";
 });
 
-/* setting popup.*/
+/* setting- rename popup.*/
 document.getElementById("rename-submit").addEventListener("click", function(){
-    let checkName = document.getElementById("name-value").value;
+    let checkName = document.getElementById("rename-value").value;
     if(!(checkName.length <= 12 && checkName.length > 0)){
         document.getElementsByClassName("settingsPop")[0].style.display = "block";
     }else{
         name = checkName;
-        document.getElementsByClassName("hello-text")[0].innerHTML = "hello, " + name + " :)";
+        document.getElementsByClassName("hello-text")[0].innerHTML = "hello, " + name + "!";
     }
+});
+
+/* close popup button*/
+document.getElementById("settings-close").addEventListener("click", function(){
+    document.getElementById("settings-popup").style.display = "none";
 });
 
 
